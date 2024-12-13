a77d6126a09f51d3b8aecca78244f1b485903ffd
Mikhail <novikovmm1981@gmail.com>
2024-12-13 23:59:50 +0300

    MyWorkingMVC

---
+++ src/main/java/web/model/CarService.java
@@ -0,0 +1,27 @@
+package web.model;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+    private List<Car> cars = new ArrayList<>();
+
+    public CarService() {
+        cars.add(new Car("Toyota", "Camry", 2020));
+        cars.add(new Car("Honda", "Civic", 2019));
+        cars.add(new Car("Ford", "Mustang", 2021));
+        cars.add(new Car("Chevrolet", "Malibu", 2018));
+        cars.add(new Car("Nissan", "Altima", 2022));
+    }
+
+    public List<Car> getCars(int count) {
+        if (count >= cars.size()) {
+            return cars;
+        }
+        return cars.subList(0, count);
+    }
+}
+
